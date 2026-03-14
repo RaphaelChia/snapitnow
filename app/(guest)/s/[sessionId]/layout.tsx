7ee41b7ac24a1926dadd8ce0313806729e553461
Raphael <clone_zone@hotmail.com>
2026-03-15 00:19:39 +0800

    added the auth provider separation

--- app/(guest)/s/[sessionId]/layout.tsx
+++ app/(guest)/s/[sessionId]/layout.tsx
@@ -1,4 +1,5 @@
-import { GuestNavbar } from "@/components/navbar";
+import { GuestNavbar } from "@/components/navbar-guest";
+import { QueryProvider } from "@/lib/query-provider";
 
 export default function GuestLayout({
   children,
@@ -6,11 +7,11 @@ export default function GuestLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <QueryProvider>
       <GuestNavbar />
       <div className="motion-safe-fade-up flex min-h-[calc(100dvh-64px)] flex-col bg-linear-to-t from-background to-muted/20 ">
         {children}
       </div>
-    </>
+    </QueryProvider>
   );
 }
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,6 @@
-import { Navbar } from "@/components/navbar"
+import { MainNavbar } from "@/components/navbar-main"
+import { AuthProvider } from "@/lib/auth/provider"
+import { QueryProvider } from "@/lib/query-provider"
 
 export default function MainLayout({
   children,
@@ -6,11 +8,13 @@ export default function MainLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-      <Navbar />
-      <div className="motion-safe-fade-up flex min-h-[calc(100dvh-4rem)] flex-col bg-linear-to-b from-background to-muted/20">
-        {children}
-      </div>
-    </>
+    <AuthProvider>
+      <QueryProvider>
+        <MainNavbar />
+        <div className="motion-safe-fade-up flex min-h-[calc(100dvh-4rem)] flex-col bg-linear-to-b from-background to-muted/20">
+          {children}
+        </div>
+      </QueryProvider>
+    </AuthProvider>
   )
 }
